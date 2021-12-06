e925106ccedeca15ebd2a082858e4ede70b91690
userkiran <88781561+userkiran@users.noreply.github.com>
2021-12-06 18:56:02 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
 }
-resource "aws_instance" "ec2" {
-ami = "ami-052cef05d01020f1d"
-instance_type = "t2.micro"
-key_name = "kmac"
-tags = {
-        Name = "jenkin-pipeline"
-}
+resource "aws_s3_bucket" "terrabucket" {
+bucket = "buckettr1234kiran"
+acl = "private"
 }
