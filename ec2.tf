745adbb4741a7d5c2ea7eb6eb5a8e2912293e166
userkiran <88781561+userkiran@users.noreply.github.com>
2021-12-08 11:57:29 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 provider "aws" {
 region = "ap-south-1"
-
+access_key = "AKIAXACQGQOCJWQR4MNQ"
+secret_key = "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"
 }
 
 resource "aws_s3_bucket" "terrabucket" {
