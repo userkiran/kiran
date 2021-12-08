1c31f41b2bb4a9043ea925355e37434915206c21
root <root@ip-172-31-41-132.ap-south-1.compute.internal>
2021-12-08 05:56:14 +0000

    s3bucket

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,10 @@
 provider "aws" {
 region = "ap-south-1"
+access_key = "AKIAXACQGQOCBXYYLF7U"
+secret_key = "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"
 }
+
 resource "aws_s3_bucket" "terrabucket" {
 bucket = "buckettr1234kiran"
+acl = "private"
 }
